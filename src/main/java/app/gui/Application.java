bdee8d8dc0ac70ecb8a0f23fc6080dd734a37377
KanTriH <kristin.han@mail.utoronto.ca>
2025-10-06 14:06:27 -0400

    finish TODO TASK4 about add action listener for get top grade

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -331,7 +331,6 @@ public class Application {
         // make a separate line.
         final JButton getAverageButton = new JButton("Get Average Grade");
         // TODO Task 4: Add another button for "Get Top Grade" (check the getAverageButton for example)
-
         final JButton leaveTeamButton = new JButton("Leave Team");
         final JLabel resultLabel = new JLabel();
 
@@ -357,6 +356,26 @@ public class Application {
         });
 
         // TODO Task 4: Add action listener for getTopGrade button, follow example of getAverageButton
+        getTopGrade.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final String course = courseField.getText();
+
+                try {
+                    final float top = getTopGradeUseCase.getTopGrade(course);
+                    JOptionPane.showMessageDialog(jFrame, "Top Grade: " + top);
+                    courseField.setText("");
+                }
+                catch (JSONException ex){
+                    JOptionPane.showMessageDialog(jFrame, ex.getMessage());
+                }
+            }
+        });
 
         leaveTeamButton.addActionListener(new ActionListener() {
             /**
@@ -379,6 +398,7 @@ public class Application {
         theCard.add(new JLabel("The course you want to calculate the team average for:"));
         theCard.add(courseField);
         theCard.add(getAverageButton);
+        theCard.add(getTopGrade);
         theCard.add(leaveTeamButton);
         theCard.add(resultLabel);
         return theCard;
